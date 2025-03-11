dde2bb068cf7477b8f0d3c818737f60788ae9cb2
Arjun Vats <arjunvats3042@gmail.com>
2025-03-12 02:01:01 +0530

    fix-try-6

--- actions/user.js
+++ actions/user.js
@@ -71,9 +71,9 @@ export async function getUserOnboardingStatus() {
   const {userId} = await auth();
   if (!userId) throw new Error("Unauthorized");
 
-  const user = await db.user.findUnique({
-    where: {clerkUserId: userId},
-  });
+  // const user = await db.user.findUnique({
+  //   where: {clerkUserId: userId},
+  // });
 
   // if (!user) throw new Error("User not found");
 

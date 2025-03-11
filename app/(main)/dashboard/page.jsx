c80bb9f11a8baa529f5180aad63bcac0a9a57082
Arjun Vats <arjunvats3042@gmail.com>
2025-03-12 02:19:38 +0530

    fix-try-8

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,57 +1,19 @@
-// import React from "react";
-// import {getUserOnboardingStatus} from "@/actions/user";
-// import {redirect} from "next/navigation";
-// import {getIndustryInsights} from "@/actions/dashboard";
-// import Dashboardview from "./_components/Dashboard-view";
-
-// const DashboardPage = async () => {
-//   const {isOnboarded} = await getUserOnboardingStatus();
-//   const insights = await getIndustryInsights();
-
-//   if (!isOnboarded) {
-//     redirect("/onboarding");
-//   }
-//   return (
-//     <div className="container ">
-//       <Dashboardview insights={insights}/>
-//     </div>
-//   );
-// };
-
-// export default DashboardPage;
-
-
 "use client";
-
-import React, {useEffect, useState} from "react";
-import {useRouter} from "next/navigation";
+import React from "react";
 import {getUserOnboardingStatus} from "@/actions/user";
+import {redirect} from "next/navigation";
 import {getIndustryInsights} from "@/actions/dashboard";
 import Dashboardview from "./_components/Dashboard-view";
 
-const DashboardPage = () => {
-  const router = useRouter();
-  const [insights, setInsights] = useState(null);
-  const [loading, setLoading] = useState(true);
+const DashboardPage = async () => {
+  const {isOnboarded} = await getUserOnboardingStatus();
+  const insights = await getIndustryInsights();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const {isOnboarded} = await getUserOnboardingStatus();
-      if (!isOnboarded) {
-        router.push("/onboarding");
-        return;
-      }
-      const insightsData = await getIndustryInsights();
-      setInsights(insightsData);
-      setLoading(false);
-    };
-    fetchData();
-  }, [router]);
-
-  if (loading) return <p>Loading...</p>;
-
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
   return (
-    <div className="container">
+    <div className="container ">
       <Dashboardview insights={insights} />
     </div>
   );
